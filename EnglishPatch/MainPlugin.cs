ea9f031c47417b557e25c88d8c4b4e591b9286e7
Josh Freitas <tabjfr@rwwa.com.au>
2025-02-28 17:11:23 +0800

    Ready to roll

--- EnglishPatch/MainPlugin.cs
+++ EnglishPatch/MainPlugin.cs
@@ -6,6 +6,7 @@ using SweetPotato;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace EnglishPatch;
 
@@ -13,29 +14,56 @@ namespace EnglishPatch;
 public class MainPlugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-
-    readonly static string ModRootPath = Path.Combine(Environment.CurrentDirectory, "BepInEx/resources");
-
-    public void Start()
-    {
-        Harmony.CreateAndPatchAll(typeof(MainPlugin));
-    }
-
+  
     private void Awake()
     {
-        // Plugin startup logic
         Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        // Plugin startup logic
+        Logger.LogWarning($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(typeof(MainPlugin));
+        Logger.LogWarning($"Plugin {MyPluginInfo.PLUGIN_GUID} should be patched!");
+    }
+    
+    public void OnDestroy()
+    {
+        Logger.LogWarning($"Plugin {MyPluginInfo.PLUGIN_GUID} is destroyed!");
     }
 
-    [HarmonyPrefix, HarmonyPatch(typeof(DataMgr), "LoadDB")]
+    [HarmonyPrefix, HarmonyPatch(typeof(SweetPotato.DataMgr), "Init")]
+    public static bool Init()
+    {
+        Logger.LogWarning($"Hooked Init!");
+        return true;
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(SweetPotato.DataMgr), "LoadDB")]
     public static bool LoadDB(DataMgr __instance, Dictionary<string, CsvLoader.CsvCreateFunc> m_dic_csv)
     {
-        Logger.LogWarning($"Current Path: {AppGame.Instance.dbVersionFilePath}");
-        AppGame.Instance.dbVersionFilePath = $"{ModRootPath}/Db1.txt";
-        Logger.LogWarning($"Loading modded file!");
+        Logger.LogWarning($"Hooked LoadDB!");
+        var resourcesFolder = Path.Combine(Environment.CurrentDirectory, "resources");
+        Logger.LogInfo($"Resources Folder: {resourcesFolder}");
+
+        //Logger.LogWarning($"Current Db File: {AppGame.Instance?.dbVersionFilePath}");
+
+        var dbFile = $"{resourcesFolder}/Db1.txt";
+        Logger.LogInfo($"New Db file: {resourcesFolder}");
+        if (File.Exists(dbFile))
+        {
+            AppGame.Instance.dbVersionFilePath = dbFile;
+            Logger.LogWarning($"Loading modded file!");
+        }
 
         // Return true to let the original LoadDB execute
+        return true;
+    }
+
+
+    [HarmonyPrefix, HarmonyPatch(typeof(ModSpace.DataMgr), nameof(ModSpace.DataMgr.LoadDB))]
+    public static bool LoadDB2(int modId, string dataName)
+    {
+        Logger.LogWarning($"ModDb2: {modId}");
         return true;
     }
 }
